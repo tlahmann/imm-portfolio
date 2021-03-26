64360df6ae537b5f690fb96032976b73d629ad06
tlahmann <tobiaslahmann@gmail.com>
2021-03-26 14:50:08 +0100

    refactor: remove many-to-many linker class
    BREAKING CHANGE: The many to many linker provides relashionships between
    the subject and the project CPT. By removing this class the
    relashionship between the two classes cannot be specified

--- imm-portfolio.php
+++ imm-portfolio.php
@@ -4,13 +4,11 @@
  */
 /*
 Plugin Name: imm Portfolio
-Description: Portfolio-Plugin für die imm Website.
-Version: 0.0.14
+Description: Portfolio-Plugin für die imm Website. Das Plugin liefert die Custom Post types <strong>Subject</strong> - Ein vom imm angebotenes Fach - <strong>Project</strong> - Ein in einem Fach durchgeführtes Projekt
+Version: 0.0.15
 Author: Tobias Lahmann
 Author URI: https://github.com/tlahmann
 License: GPLv2 or later
-Last Modifications: 
-    restructure classes
 */
 
 // Store the current plugin path in a global variable
@@ -30,8 +28,3 @@ new Subject();
 // Call an instance from the project class
 require_once( IMM__PLUGIN_DIR . 'Project.class.php' );
 new Project();
-
-if ( is_admin() ) {
-    require_once( IMM__PLUGIN_DIR . 'ManyToManyLinker.class.php' );
-    new ManyToManyLinker();
-}
